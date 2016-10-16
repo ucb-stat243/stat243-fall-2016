24c7a520d8bc65b649d822be8812c2815c74b193
Gaston Sanchez <gaston.stat@gmail.com>
2016-10-15 20:56:15 -0700

    examples: add R script with S3 classes for die rolling

---
+++ examples/die-roll/die-roll-s3class.R
@@ -0,0 +1,166 @@
+
+
+# auxiliar function to check the sides of a die
+check_sides <- function(sides) {
+  if (length(sides) != 6 | !is.numeric(sides)) {
+    stop("\n'sides' must be a numeric vector of length 6")
+  }
+  TRUE
+}
+
+
+# auxiliar function to check probabilities of the sides of a die
+check_prob <- function(prob) {
+  if (length(prob) != 6 | !is.numeric(prob)) {
+    stop("\n'prob' must be a numeric vector of length 6")
+  }
+  if (any(prob < 0) | any(prob > 1)) {
+    stop("\n'prob' values must be between 0 and 1")
+  }
+  if (sum(prob) != 1) {
+    stop("\nelements in 'prob' must add up to 1")
+  }
+  TRUE
+}
+
+
+# class "die"
+die <- function(sides = 1:6, prob = rep(1/6, 6)) {
+  check_sides(sides)
+  check_prob(prob)
+  object <- list(
+    sides = sides,
+    prob = prob
+  )
+  class(object) <- "die"
+  object
+}
+
+
+# auxiliar function to check 'times'
+check_times <- function(times) {
+  if (times <= 0 | !is.numeric(times)) {
+    stop("\nargument 'times' must be a positive integer")
+  } else {
+    TRUE
+  }
+}
+
+# auxiliar function to roll a die
+roll_die <- function(x, times = 1) {
+  sample(x$sides, size = times, replace = TRUE, prob = x$prob)
+}
+
+
+# constructor function for object "roll"
+make_roll <- function(die, rolls) {
+  object <- list(
+    rolls = rolls,
+    sides = die$sides,
+    prob = die$prob,
+    total = length(rolls))
+  class(object) <- "roll"
+  object
+}
+
+
+# main roll function
+roll <- function(die, times = 1) {
+  rolls <- roll_die(die, times = times)
+  make_roll(die, rolls)
+}
+
+
+# print method for object of class "roll"
+print.roll <- function(x, ...) {
+  cat('object "roll"\n\n')
+  cat('$rolls\n')
+  print(x$rolls)
+  invisible(x)
+}
+
+
+# summary method for object of class "roll"
+summary.roll <- function(x, ...) {
+  freqs <- table(x$rolls)
+  relative_freqs <- prop.table(freqs)
+  # table of frequencies
+  roll_freqs <- as.data.frame(cbind(
+    side = x$sides, 
+    count = freqs, 
+    prop = relative_freqs))
+  obj <- list(freqs = roll_freqs)
+  class(obj) <- "summary.roll"
+  obj
+}
+
+
+# print summary method for object of class "roll"
+print.summary.roll <- function(x, ...) {
+  cat('summary "roll"\n\n')
+  print(x$freqs)
+  invisible(x)
+}
+
+
+
+# plot method for objects of class "roll"
+plot.roll <- function(x, ...) {
+  freqs <- prop.table(table(x$rolls))
+  barplot(freqs, border = NA, las = 1,
+          xlab = "sides of die",
+          ylab = "relative frequencies")
+  title(sprintf("Frequencies in a series of %s die rolls", length(x$rolls)))
+}
+
+
+# replacement method for "roll"
+"[<-.roll" <- function(x, i, value) {
+  if (all(x$sides != value)) {
+    stop(paste('\nreplacing value must be one of', x$sides))
+  }
+  if (i > length(x$rolls)) {
+    stop("\nindex out of bounds")
+  }
+  x$rolls[i] <- value
+  x
+}
+
+
+# extraction method for objects of class "roll"
+"[.roll" <- function(x, i) {
+  x$rolls[i]
+}
+
+
+# addition method for class "roll"
+"+.roll" <- function(obj, incr) {
+  if (length(incr) != 1 | incr <= 0) {
+    stop("\ninvalid increament")
+  }
+  more_rolls <- roll_die(obj, times = incr)
+  obj$rolls <- c(obj$rolls, more_rolls)
+  obj$total <- length(obj$rolls)
+  obj
+}
+
+
+# check class "roll"
+is.roll <- function(x) {
+  inherits(x, "roll")
+}
+
+
+# chi square statistic for an object "roll"
+chi_square <- function(x) {
+  obs <- prop.table(table(x$rolls))
+  exp <- 1/6
+  chi <- length(x$rolls) * sum( ((obs - exp)^2) / exp )
+  list(
+    stat = chi,
+    df = 5
+  )
+}
+
+
+
